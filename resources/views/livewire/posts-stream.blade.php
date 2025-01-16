c4531358468df6c6f91f849d8323f02e00636966
Harvs04 <hamartinez@up.edu.ph>
2025-01-16 22:54:04 +0800

    feat: prevent ordering in own transaction

--- resources/views/livewire/posts-stream.blade.php
+++ resources/views/livewire/posts-stream.blade.php
@@ -321,7 +321,7 @@
                                             <p class="hidden lg:font-medium lg:block" :class="openBurger ? 'hidden lg:block lg:text-xs xl:text-sm' : 'md:block'">I want this too!</p>
                                         </div>
                                     </button>
-                                    <button class="w-4/12 md:w-5/12 py-1.5 ml-auto enabled:hover:bg-gray-200 enabled:hover:rounded-md disabled:bg-gray-300 disabled:text-gray-400 disabled:rounded-md" :class="'{{ $user->role }}' === 'provider' ? 'hidden' : 'block'">
+                                    <button class="w-4/12 md:w-5/12 py-1.5 ml-auto enabled:hover:bg-gray-200 enabled:hover:rounded-md disabled:bg-gray-300 disabled:text-gray-400 disabled:rounded-md" :class="'{{ $user->role }}' === 'provider' ? 'hidden' : 'block'" :disabled="'{{ $user->id }}' === '{{ $post->user_id }}'">
                                         <div class="flex flex-row items-center justify-center gap-3">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-4 md:size-5">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
