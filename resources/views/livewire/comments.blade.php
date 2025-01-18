13aeaa77b5127ca3f47ec415cc0ac7593cb825d4
Harvs04 <hamartinez@up.edu.ph>
2025-01-18 14:21:31 +0800

    feat: date in comments

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,21 +1,30 @@
 <div>
     <hr class="my-2">
-    <div class="px-3 py-1">
+    <div class="py-1">
         <div class="" x-data="{ comment: '', comments: $wire.entangle('comments')  }">
             
             <!-- COMMENTS STREAM -->
             @if($db_comments->count() > 0) 
                 <div class="flex flex-col gap-2">
                     @foreach($db_comments as $comment)
-                        <div class="flex flex-row items-start gap-3 md:gap-4">
+                        <div class="flex flex-row items-start gap-3 md:gap-4" x-data="{ showDate: false }">
                             <img class="w-8 md:w-9 h-8 md:h-9 rounded-full object-cover" 
                                 src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" 
                                 alt="user photo">
-                            <div class="flex flex-col items-start">
-                                <div class="flex flex-row gap-2 items-center">
+                            <div class="flex flex-col items-start w-full">
+                                <div class="flex flex-row gap-2 items-center relative">
                                     <p class="font-medium">{{ $comment->commenter }}</p>
-                                    <p class="text-xs">{{ $comment->created_at->diffForHumans(null, false, true) }}</p>
-                                </div>        
+                                    <p class="text-xs hover:underline" 
+                                        @mouseenter="showDate = true" 
+                                        @mouseleave="showDate = false">
+                                        {{ $comment->created_at->diffForHumans(null, false, true) }}
+                                    </p>
+                                    <div x-show="showDate" 
+                                        class="text-xs absolute -top-14 left-0 z-50 border rounded-lg bg-gray-200 text-gray-700 shadow px-2.5 py-2">
+                                        <p>{{ $comment->created_at->timezone('Singapore')->format('l F j, Y \a\t H:i') }}</p>       
+                                    </div>
+                                </div>
+
                                 <p class="text-sm break-all whitespace-pre-line">{{$comment->comment}}</p>
                             </div>
                         </div>
