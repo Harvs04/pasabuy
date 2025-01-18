ba6231389b93eabc5df308181d604cb47e055d62
Harvs04 <hamartinez@up.edu.ph>
2025-01-18 16:56:38 +0800

    fix: comment on transactions

--- resources/views/livewire/posts-stream.blade.php
+++ resources/views/livewire/posts-stream.blade.php
@@ -336,7 +336,7 @@
                                                     <p class="hidden lg:font-medium lg:block" :class="openBurger ? 'hidden md:block md:text-sm lg:text-xs xl:text-sm' : 'md:block'">Order item</p>
                                                 </div>
                                             </button>
-                                            <button @click="openComment = !openCommentclass="w-4/12 md:w-5/12 py-1.5 enabled:hover:bg-gray-200 enabled:hover:rounded-md disabled:bg-gray-300 disabled:text-gray-400 disabled:rounded-md">
+                                            <button @click="openComment = !openComment" class="w-4/12 md:w-5/12 py-1.5 enabled:hover:bg-gray-200 enabled:hover:rounded-md disabled:bg-gray-300 disabled:text-gray-400 disabled:rounded-md">
                                                 <div class="flex flex-row items-center justify-center gap-3">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-4 md:size-5" :class="openBurger ? 'lg:size-4 xl:size-5': 'size-5'">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
@@ -355,9 +355,8 @@
                                         </div>
                                     </div>
                                 </div>
-                                {{ $post->id }}
                                 <div x-show="openComment">
-                                    <livewire:comments :$post :key="$post->id"/>
+                                    <livewire:comments :post="$post"/>
                                 </div>
                             </div>
                         </div>
