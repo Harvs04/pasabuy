5a1d48f94e99c2f33953ccc085d91de831b9950b
Harvs04 <hamartinez@up.edu.ph>
2025-01-29 14:39:57 +0800

    fix: fetching of transactions

--- app/Livewire/Transaction.php
+++ app/Livewire/Transaction.php
@@ -5,14 +5,15 @@ namespace App\Livewire;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Order;
+use App\Models\Post;
 
 class Transaction extends Component
 {
     use WithPagination;
     public $id;
     public function render()
-    {
-        $orders = Order::where('post_id', $this->id)->orderByDesc('created_at')->paginate(10);
-        return view('livewire.transaction', ['orders' => $orders]);
+    {   $transaction = Post::where('id', $this->id)->first();
+        $orders = Order::where('post_id', $transaction->id)->orderByDesc('created_at')->paginate(10);
+        return view('livewire.transaction', ['transaction' => $transaction, 'orders' => $orders]);
     }
 }
