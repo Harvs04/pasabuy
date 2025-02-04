687d1b812d4bbcad05092365f3b20d57ad8968fe
Harvs04 <hamartinez@up.edu.ph>
2025-02-04 22:10:49 +0800

    feat: rendering of orders of a transaction

--- app/Livewire/Orders.php
+++ app/Livewire/Orders.php
@@ -4,15 +4,24 @@ namespace App\Livewire;
 
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use App\Models\Post;
+use Livewire\WithPagination;
 
 class Orders extends Component
 {
+    use WithPagination;
 
     public $user; 
     
     public function render()
     {
         $this->user = Auth::user();
-        return view('livewire.orders', ['user' => $this->user]);
+        // Extract post IDs from the user's orders
+        $postIds = collect($this->user->orders)->pluck('post_id')->toArray();
+
+        // Retrieve the posts matching these IDs
+        $transactions = Post::whereIn('id', $postIds)->paginate('10');
+
+        return view('livewire.orders', ['user' => $this->user, 'transactions' => $transactions]);
     }
 }
