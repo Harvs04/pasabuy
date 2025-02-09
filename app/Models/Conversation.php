1baa295ac197dc3c7cddedf8c6abbe7eb666c773
Harvs04 <hamartinez@up.edu.ph>
2025-02-09 14:55:15 +0800

    feat: conversation + message tables

---
+++ app/Models/Conversation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'post_id',
+        'provider_id',
+        'customer_id'
+    ];
+}
