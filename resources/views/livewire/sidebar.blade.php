18ba72a46d2208b42d1b478721f6ae37100961b2
Harvs04 <hamartinez@up.edu.ph>
2025-02-23 19:02:15 +0800

    removed: search in sidebar in mobile

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -12,16 +12,6 @@
       <div class="h-full px-3 pb-4 overflow-y-auto">
          <ul class="space-y-2 text-[15px]">
             <li>
-               <div class="relative block w-full sm:hidden mt-2">
-                  <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg class="w-3 h-3 sm:w-4 sm:h-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                  </svg>
-                  </div>
-                  <input type="text" id="search-sidebar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50" placeholder="Search items, stores, ...">
-               </div>
-            </li>
-            <li>
                <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg {{ Request::routeIs('dashboard') ? 'bg-gray-100' : 'hover:bg-gray-100 group hover:font-medium' }}">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="flex-shrink-0 w-5 h-5 sm:w-6 sm:h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 {{ Request::routeIs('dashboard') ? 'text-gray-900' : '' }}">
                      <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
