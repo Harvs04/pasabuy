0def02a98d9cdceaab6f258fd78806f61ef788e2
Harvs04 <hamartinez@up.edu.ph>
2025-02-26 20:54:28 +0800

    feat: added loading for rating transaction in order list

--- resources/views/livewire/order-list.blade.php
+++ resources/views/livewire/order-list.blade.php
@@ -102,7 +102,7 @@
     <livewire:sidebar />
 
     @teleport('body')
-    <div wire:loading.delay wire:target="confirmDelivery, cancelOrder"
+    <div wire:loading.delay wire:target="confirmDelivery, rateTransaction, cancelOrder"
         class="fixed inset-0 bg-white bg-opacity-50 z-50 flex items-center justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101"
             class="w-12 h-12 text-gray-200 animate-spin fill-[#014421]"
