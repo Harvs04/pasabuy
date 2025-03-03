fd6056bbecaa33da3de5f13818e795d65f8fd4a4
Harvs04 <hamartinez@up.edu.ph>
2025-03-03 23:27:43 +0800

    fix: position

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -141,7 +141,7 @@
          }, 3000); // 3 seconds
       </script>
    @endif
-   <div class="fixed bottom-4 right-4 z-10 lg:hidden">
+   <div class="fixed bottom-4 right-20 z-10 lg:hidden">
       <button @click="openFilter = true" class="bg-[#014421] hover:bg-green-800 shadow-lg rounded-full w-12 h-12 flex items-center justify-center">
          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF" class="size-5">
             <path fill-rule="evenodd" d="M3.792 2.938A49.069 49.069 0 0 1 12 2.25c2.797 0 5.54.236 8.209.688a1.857 1.857 0 0 1 1.541 1.836v1.044a3 3 0 0 1-.879 2.121l-6.182 6.182a1.5 1.5 0 0 0-.439 1.061v2.927a3 3 0 0 1-1.658 2.684l-1.757.878A.75.75 0 0 1 9.75 21v-5.818a1.5 1.5 0 0 0-.44-1.06L3.13 7.938a3 3 0 0 1-.879-2.121V4.774c0-.897.64-1.683 1.542-1.836Z" clip-rule="evenodd" />
@@ -337,6 +337,9 @@
 
    <!-- SIDEBAR -->
    <livewire:sidebar />
+
+   <!-- CHATBOT -->
+   <livewire:chatbot />
    
    <!-- MAIN CONTENT -->
    <div class="sm:transition-all sm:duration-300 sm:transform relative" style="margin-top: 4.3rem;"
