3c6b42f2139737cae77dd2a1d7bf33f66062bfa7
Harvs04 <hamartinez@up.edu.ph>
2025-03-09 23:41:05 +0800

    fix: added suffix 1 for each question of user

--- resources/views/livewire/chatbot.blade.php
+++ resources/views/livewire/chatbot.blade.php
@@ -85,7 +85,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bot"><path d="M12 8V4H8"/><rect width="16" height="12" x="4" y="8" rx="2"/><path d="M2 14h2"/><path d="M20 14h2"/><path d="M15 13v2"/><path d="M9 13v2"/></svg>
                                 </div>
                                 <div class="mt-0.5">
-                                    <button class="px-2 border rounded-md hover:bg-gray-100" @click="stayInCurrent = true; if (!conversation.includes('stay here1')) { conversation.push('stay here1'); $wire.add_message('stay here1'); }">
+                                    <button class="px-2 border rounded-md hover:bg-gray-100" @click="stayInCurrent = true; conversation.push('stay here1'); $wire.add_message('stay here1'); ">
                                         Stay here
                                     </button>
                                     @foreach($routes as $route => $link)
