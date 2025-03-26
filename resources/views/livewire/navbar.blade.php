92674b690c8834ea8a52daadc09d9ca9d554b846
Harvs04 <hamartinez@up.edu.ph>
2025-03-26 17:15:44 +0800

    feat: moved the modal for notif in navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -43,7 +43,7 @@
                      <h1 class="text-lg md:text-xl font-semibold text-gray-800 ml-1">Notifications</h1>
                      <div class="mt-4">
                         @forelse ($user->notification_as_poster as $notif)
-                           <button class="flex flex-row gap-3 p-1 hover:bg-gray-100 hover:rounded text-sm justify-start items-start w-full" @click="notifDetailsOpen = true; document.body.style.overflow = 'hidden'; @wire.openNotif('{{ $notif->id }}');">
+                           <button class="flex flex-row gap-3 p-1 hover:bg-gray-100 hover:rounded text-sm justify-start items-start w-full" @click="notifDetailsOpen = true; document.body.style.overflow = 'hidden'; $wire.openNotif('{{ $notif->id }}');">
                               <img class="size-9 md:size-12 flex-shrink-0 object-contain rounded-full border" src="{{ App\Models\User::where('id', $notif->actor_id)->first()->profile_pic_url }}" alt="user photo"> 
                               <div class="flex flex-col items-start">
                                  <span class="font-medium leading-tight text-start">
@@ -87,16 +87,38 @@
                                  <span class="text-xs">{{ $notif->created_at->Timezone('Singapore')->format('F j, Y \\a\\t h:i A') }}</span>
                               </div>
                            </button>
-                           @teleport('body')
-                              <div x-show="notifDetailsOpen">
-                                 {{ $post_id }}
-                              </div>
-                           @endteleport
                         @empty
                            <p class="text-sm text-center">
                               Seems empty. Try ordering items or making transactions.
                            </p>
                         @endforelse
+                        <p>NOTIF ID: {{ $notif_id }}</p>
+                        @teleport('body')
+                           <div x-show="notifDetailsOpen">
+                              <div x-transition:enter.duration.25ms class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-30">
+                                 <div @keydown.escape.window="notifDetailsOpen = false; document.body.style.overflow = 'auto';" class="bg-white p-6 rounded-lg w-11/12 md:w-2/3 lg:w-1/2 2xl:w-1/3 relative">
+                                    <div class="flex flex-row items-center gap-2 sm:gap-3">
+                                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#014421" class="size-6 md:size-7">
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                       </svg>
+                                       <p class="text-xl font-semibold text-[#014421]">Confirmation</p>
+                                       <button @click="notifDetailsOpen = false; document.body.style.overflow = 'auto';" class="absolute top-4 right-4 p-2 hover:bg-gray-100 hover:rounded-full">
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#000000" class="size-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                          </svg>
+                                       </button>
+                                    </div>
+                                    <div>
+                                       HELLO YOU CLICKED NOTIF# {{ $notif_id }}
+                                    </div>
+                                    <div  class="mt-5 flex justify-end gap-2">
+                                       <button @click="notifDetailsOpen = false; document.body.style.overflow = 'auto';" class="font-medium px-2 sm:px-3 py-1.5 text-sm bg-white text-black  rounded-md hover:bg-slate-200 border hover:border-slate-200 hover:text-black">Cancel</button>
+                                       <button @click="" class=" px-2 sm:px-3 py-1 sm:py-1.5 text-sm bg-[#014421] text-white rounded-md hover:bg-green-800">Confirm</button>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        @endteleport
                      </div>
                   </div>
                </div>
