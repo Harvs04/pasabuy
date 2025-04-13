a6d761ddee676d581f8e91305225593f31a65b1e
Harvs04 <hamartinez@up.edu.ph>
2025-04-13 19:35:56 +0800

    feat: report ui in dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -140,6 +140,27 @@
             document.querySelector('.flash').style.display = 'none';
          }, 3000); // 3 seconds
       </script>
+   @elseif(session('report_user_success'))
+      <div class="flash fixed top-8 left-1/2 transform -translate-x-1/2 z-50 bg-[#014421] border-t border-white text-white px-1.5 py-1 w-4/6 md:w-fit max-w-md flex justify-center items-center rounded-lg shadow-sm sm:shadow-md">
+         <div class="flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+
+            <div class="text-center text-sm">
+               {{ session('report_user_success') }}
+            </div>
+         </div>
+         <!-- Close Button -->
+         <button onclick="this.parentElement.style.display='none'" class="text-white font-bold p-2 ml-auto">
+            &times;
+         </button>
+      </div>
+      <script>
+         setTimeout(() => {
+            document.querySelector('.flash').style.display = 'none';
+         }, 3000); // 3 seconds
+      </script>
    @endif
    <div class="fixed bottom-4 right-20 z-10 lg:hidden">
       <button @click="openFilter = true" class="bg-[#014421] hover:bg-green-800 shadow-lg rounded-full w-12 h-12 flex items-center justify-center transform transition-transform duration-300 hover:scale-110">
@@ -331,6 +352,22 @@
    <div wire:loading wire:target="applyFilter, clearFilter"  class="w-full">
       <livewire:dashboard-skeleton />
    </div>
+   
+   @teleport('body')
+   <div wire:loading wire:target="reportUser"
+      class="fixed inset-0 bg-white bg-opacity-50 z-50 flex items-center justify-center">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101"
+         class="w-12 h-12 text-gray-200 animate-spin fill-[#014421]"
+         style="position: absolute; top: 50%; left: 50%;">
+         <path
+               d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+               fill="currentColor" />
+         <path
+               d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+               fill="currentFill" />
+      </svg>
+   </div>
+   @endteleport
 
    <!-- NAVBAR --> 
    <livewire:navbar />
