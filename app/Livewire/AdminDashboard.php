cef16d60133f6e2f9af8557a08259ce4167497bb
Harvs04 <hamartinez@up.edu.ph>
2025-04-16 22:24:50 +0800

    feat: user in Admin

--- app/Livewire/AdminDashboard.php
+++ app/Livewire/AdminDashboard.php
@@ -3,9 +3,18 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class AdminDashboard extends Component
 {
+    public $user;
+
+    public function __construct()
+    {
+        $this->user = Auth::user();
+    }
+
+
     public function render()
     {
         return view('livewire.admin-dashboard');
