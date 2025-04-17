e028dadf8dc6fefb66ebc0e2f1fdc2710ebe11c0
Harvs04 <hamartinez@up.edu.ph>
2025-04-17 19:41:25 +0800

    fix extension name

---
+++ resources/views/admin/report-list.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="x-icon" href="https://res.cloudinary.com/dflz6bik9/image/upload/v1738234575/Pasabuy-logo-no-name_knwf3t.png">
+    <title>Pasabuy</title>
+    @vite('resources/css/app.css')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    @livewireStyles
+</head>
+<body class="bg-gray-100">
+    <livewire:report-list />
+    @livewireScripts
+    @livewireScriptConfig
+</body>
+</html>
