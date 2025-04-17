6f3f77bf31268620282543aa646ac2bc0267b607
Harvs04 <hamartinez@up.edu.ph>
2025-04-17 19:56:29 +0800

    fix: ui in admin dashboard

--- resources/views/livewire/admin-dashboard.blade.php
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -5,30 +5,41 @@
     <!-- NAVBAR -->
     <livewire:navbar />
 
-    <!-- SIDEBAR -->
-    <livewire:sidebar />
-
     <div class="sm:transition-all sm:duration-300 sm:transform relative flex-grow flex items-center justify-center" style="margin-top: 4.3rem;"
     wire:loading.class="hidden"
     wire:target="switchRole, applyFilter, clearFilter"
     >   
         <div class="flex flex-col items-center justify-center w-full max-w-4xl px-4">
-            <div class="text-center w-full md:w-2/3 p-5 rounded-md bg-white shadow-sm border">
-                <p class="text-gray-800 text-xl font-semibold">Welcome back, Admin {{ $user->name }}</p>
+            <div class="text-center w-full md:w-9/12 p-6 rounded-md bg-white shadow-sm border">
+                <p class="text-black text-2xl font-medium">Welcome back, {{ $user->name }}!</p>
             </div>
-            <div class="w-full md:w-2/3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
-                <button class="text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300">
-                    <p class="text-gray-600 text-lg font-medium">Users list</p>
-                </button>
-                <button class="text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300">
-                    <p class="text-gray-600 text-lg font-medium">Posts list</p>
-                </button>
-                <button class="text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300">
-                    <p class="text-gray-600 text-lg font-medium">Orders list</p>
-                </button>
-                <button class="text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300">
-                    <p class="text-gray-600 text-lg font-medium">Reports list</p>
-                </button>
+            <div class="w-full md:w-11/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4 text-gray-700">
+                <a href="{{ route('user-list') }}" class="flex gap-1.5 items-center justify-center text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300 hover:text-gray-800 hover:cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                        <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                    </svg>
+                    <p class="text-lg font-medium">Users list</p>
+                </a>
+                <a href="{{ route('post-list') }}" class="flex gap-1.5 items-center justify-center text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300 hover:text-gray-800 hover:cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                        <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                        <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                    </svg>
+                    <p class="text-lg font-medium">Posts list</p>
+                </a>
+                <a href="{{ route('order-list') }}" class="flex gap-1.5 items-center justify-center text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300 hover:text-gray-800 hover:cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                        <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clip-rule="evenodd" />
+                        <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+                    </svg>
+                    <p class="text-lg font-medium">Orders list</p>
+                </a>
+                <a href="{{ route('report-list') }}" class="flex gap-1.5 items-center justify-center text-center w-full p-5 rounded-md bg-white shadow-sm border transform hover:scale-105 duration-300 hover:text-gray-800 hover:cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5">
+                        <path fill-rule="evenodd" d="M3 2.25a.75.75 0 0 1 .75.75v.54l1.838-.46a9.75 9.75 0 0 1 6.725.738l.108.054A8.25 8.25 0 0 0 18 4.524l3.11-.732a.75.75 0 0 1 .917.81 47.784 47.784 0 0 0 .005 10.337.75.75 0 0 1-.574.812l-3.114.733a9.75 9.75 0 0 1-6.594-.77l-.108-.054a8.25 8.25 0 0 0-5.69-.625l-2.202.55V21a.75.75 0 0 1-1.5 0V3A.75.75 0 0 1 3 2.25Z" clip-rule="evenodd" />
+                    </svg>
+                    <p class="text-lg font-medium">Reports list</p>
+                </a>
             </div>
         </div>
     </div>
