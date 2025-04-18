a69d7311a8ba9035b9d96db82204e6bac81e4d63
Harvs04 <hamartinez@up.edu.ph>
2025-04-18 22:57:14 +0800

    feat: delete order in admin

--- app/Livewire/OrdersList.php
+++ app/Livewire/OrdersList.php
@@ -5,11 +5,16 @@ namespace App\Livewire;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
+use App\Models\User;
+use App\Models\Notification;
 
 class OrdersList extends Component
 {
     public $user;
     public $orders;
+    public $customer_id;
+    public $provider_id;
+
 
     public function __construct()
     {
@@ -17,8 +22,60 @@ class OrdersList extends Component
         $this->orders = Order::all();
     }
 
+
+    public function deleteOrder($order_id)
+    {   
+        try {
+            $orderToDelete = Order::where('id', $order_id)->first();
+
+            if (!$orderToDelete) {
+                session()->flash('error', 'order not found.');
+                return $this->redirect(route('order-list'));
+            }
+
+            $notifications = Notification::whereJsonContains('order_id', $order_id)->get();
+
+            foreach ($notifications as $notification) {
+                if ($notification->order_count === 1) {
+                    $notification->delete();
+                } else {
+                    $orderIds = $notification->order_id; // Get current array
+            
+                    // Remove the specific order_id
+                    $orderIds = array_filter($orderIds, function ($id) use ($order_id) {
+                        return $id != $order_id;
+                    });
+                    $notification->order_id = array_values($orderIds); // Reindex
+                    $notification->order_count -= 1;
+                    $notification->save();
+                }
+            }
+
+            $orderToDelete->delete();
+            session()->flash('order_deleted', 'order deleted successfully.');
+            return $this->redirect(route('order-list'));
+        } catch (\Exception $e) {
+            session()->flash('error', 'An error occurred while deleting the order: ' . $e->getMessage());
+            return $this->redirect(route('order-list'));
+        } 
+    }
+
     public function render()
     {
-        return view('livewire.orders-list');
+        $customer = null;
+        $provider = null;
+
+        if ($this->customer_id !== null) {
+            $customer = User::where('id', $this->customer_id)->first();
+        } 
+
+        if ($this->provider_id !== null) {
+            $provider = User::where('id', $this->provider_id)->first();
+        }
+
+        return view('livewire.orders-list', [
+            'customer' => $customer,
+            'provider' => $provider
+        ]);
     }
 }
