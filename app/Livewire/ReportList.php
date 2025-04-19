0fe9dd2805694e7cc2778743d568e529c7305ad7
Harvs04 <hamartinez@up.edu.ph>
2025-04-19 17:29:43 +0800

    fix: removed conditional for status

--- app/Livewire/ReportList.php
+++ app/Livewire/ReportList.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Report;
 use App\Models\User;
+use App\Models\Post;
 
 class ReportList extends Component
 {   
@@ -14,6 +15,7 @@ class ReportList extends Component
     public $reports;
     public $sender_id;
     public $reported_id;
+    public $post_id;
 
     public function __construct()
     {
@@ -35,28 +37,25 @@ class ReportList extends Component
             $selected_report->status = $status;
             $selected_report->save();
 
-            if ($status === 'Resolved') {
-                $sender_user = User::where('id', $this->sender_id)->first();
-                $sender_user->pasabuy_points += $points_add;
-                $sender_user->save();
+            $sender_user = User::where('id', $this->sender_id)->first();
+            $sender_user->pasabuy_points += $points_add;
+            $sender_user->save();
 
-                $reported_user = User::where('id', $this->reported_id)->first();
-                $reported_user->pasabuy_points -= $points_minus;
-                $reported_user->save();
+            $reported_user = User::where('id', $this->reported_id)->first();
+            $reported_user->pasabuy_points -= $points_minus;
+            $reported_user->save();
 
-                Notification::create([
-                    'type' => 'report resolved',
-                    'post_id' => $selected_report->post_id,
-                    'actor_id' => $selected_report->sender_id,
-                    'order_id' => $selected_report->id,
-                    'poster_id' => $selected_report->sender_id,
-                ]);
-            }
+            Notification::create([
+                'type' => 'report resolved',
+                'post_id' => $selected_report->post_id,
+                'actor_id' => $selected_report->sender_id,
+                'order_id' => $selected_report->id,
+                'poster_id' => $selected_report->sender_id,
+            ]);
 
             session()->flash('report_updated', 'Report updated successfully.');
             return $this->redirect(route('report-list'));
         } catch (\Exception $e) {
-            dd($e);
             session()->flash('error', 'report not found.');
             return $this->redirect(route('report-list'));
         } 
@@ -66,6 +65,7 @@ class ReportList extends Component
     {
         $sender = null;
         $reported = null;
+        $post = null;
 
         if ($this->sender_id !== null) {
             $sender = User::where('id', $this->sender_id)->first();
@@ -75,9 +75,14 @@ class ReportList extends Component
             $reported = User::where('id', $this->reported_id)->first();
         }
 
+        if ($this->post_id !== null) {
+            $post = Post::where('id', $this->post_id)->first();
+        }
+
         return view('livewire.report-list', [
             'sender' => $sender,
-            'reported' => $reported
+            'reported' => $reported,
+            'post' => $post
         ]);
     }
 }
