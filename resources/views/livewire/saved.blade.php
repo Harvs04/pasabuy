761bcdcbfaf2b1730b54a1d7cd60b7f3c89d3d81
Harvs04 <hamartinez@up.edu.ph>
2025-05-01 17:12:00 +0800

    fix: ui for banner messages in dashboard + saved

--- resources/views/livewire/saved.blade.php
+++ resources/views/livewire/saved.blade.php
@@ -227,7 +227,7 @@
             <div class="flex flex-col gap-2 md:gap-4">
                 @if ($user->contact_number === null || $user->constituent === null || $user->college === null ||
                 $user->degree_program === null)
-                <div class="flex flex-col w-full rounded-md bg-rose-200 px-3 py-2.5 text-[#7b1113] gap-2">
+                <div class="flex flex-col w-full border-red-800 md:border-l-4 p-2.5 md:rounded-r-md shadow md:shadow-md bg-rose-200 px-3 py-2.5 text-[#7b1113] gap-2">
                     <div class="flex flex-row items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" viewBox="0 0 20 20"
                             fill="currentColor">
@@ -251,7 +251,7 @@
                 </div>
                 @endif
                 @if ($user->pasabuy_points < 80) <div
-                    class="flex flex-col w-full rounded-md bg-rose-200 px-3 py-2.5 text-[#7b1113] gap-2">
+                    class="flex flex-col w-full border-red-800 md:border-l-4 p-2.5 md:rounded-r-md shadow md:shadow-md bg-rose-200 px-3 py-2.5 text-[#7b1113] gap-2">
                     <div class="flex flex-row items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" viewBox="0 0 20 20"
                             fill="currentColor">
@@ -259,7 +259,7 @@
                                 d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <p class="text-sm sm:text-base font-medium">
+                        <p class="text-sm sm:text-base font-semibold">
                             Insufficient PASABUY points
                         </p>
                     </div>
